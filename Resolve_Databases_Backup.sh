3299317e80b2b0806bf43bdd3dbc515bf56ea5ed
flamescripts <47398752+flamescripts@users.noreply.github.com>
2020-12-12 13:44:09 -0800

    Update Resolve_Databases_Backup.sh
    removed color output, minor fixes.

--- Resolve_Databases_Backup.sh
+++ Resolve_Databases_Backup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# Davinci Resolve postgres database backup for use with macOS.
-# Postgres passwords need to be stored in user ~/.pgpass
-# DBnames is in line wth DBips 1:1 2:2 etc.
-# Usage: 
-#   DBnames=('name_of_dbase1' 'name_of_dbase2' 'name_of_dbase3')
-#   DBips=('ip_for_dbase1' 'ip_for_dbase2' 'ip_for_dbase3')
-# Author not responsible for any data loss from use.  Do not use
-# unless you understand every command and it's risk.
-# Data loss possible.
+#  Davinci Resolve postgres database backup for use with macOS.
+#  Postgres passwords need to be stored in user ~/.pgpass
+#
+#  DBnames is in line wth DBips 1:1 2:2 etc.
+#  Usage: 
+#     DBnames=('name_of_dbase1' 'name_of_dbase2' 'name_of_dbase3')
+#     DBips=('ip_for_dbase1' 'ip_for_dbase2' 'ip_for_dbase3')
+#
+#  Author not responsible for any data loss from use.
+#  Do not use unless you understand every command and it's risk.
+#  Data loss possible.
 
 # Arrays and Variables
 DBnames=('pub_db_2020' 'mv_db_2020' 'comm_db_2020')
@@ -33,47 +35,51 @@ do
 	DBname=${DBnames[$db]}
 	DBtaskcurrent=$((db+1))
 	DBtasktotal=$((${#DBnames[@]}+1))
-	DBbacknum=$(ls -1t $RSYNCpath | grep $DBname | grep $DBip | wc -l)
+	DBbacknum=$(ls -1t $BUdatabases | grep $DBname | grep $DBip | wc -l)
 
 	#Tally processes and report current task
-	echo -e "Processing Job $DBtaskcurrent of $DBtasktotal: Checking Database: ${DBname} on IP: ${DBip}"
+	echo "Processing Job $DBtaskcurrent of $DBtasktotal: Checking Database: ${DBname} on IP: ${DBip}"
 
 	# This should have tighter conditionals.
-  #	if [ $(ping -c 2 -W 1 ${DBip} | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }') -eq 2 ];
 	if /Library/PostgreSQL/12/bin/pg_isready -d postgres://${DBip}:5432/template;
 	then
 		# DB Online, Log Event and Proceed
 		echo -e "`logger -p user.debug -s -t "${DBip} > ${DBname}: Online " "DaVinci Resolve Database Backup Started" 2>&1 | tee -a $BUpath/backup_debug.tmp`"
 
 		# Maintanance and backup of postgres
-		/Library/PostgreSQL/12/bin/reindexdb --host ${DBip} --username postgres ${DBname} --no-password --echo &>/dev/null
-		/Library/PostgreSQL/12/bin/vacuumdb --analyze --host ${DBip} --username postgres ${DBname} --verbose --no-password &>/dev/null
-		/Library/PostgreSQL/12/bin/pg_dump --host ${DBip} --username postgres ${DBname} --blobs --file $BUdatabases/Resolve_${DBname}_${DBip}_$(date +'%m-%d-%Y_%H-%M')_PostgreSQL_db.backup --verbose --format=custom --compress=9 --no-password &>/dev/null
+#		/Library/PostgreSQL/12/bin/reindexdb --host ${DBip} --username postgres ${DBname} --no-password --echo &>/dev/null
+#		/Library/PostgreSQL/12/bin/vacuumdb --analyze --host ${DBip} --username postgres ${DBname} --verbose --no-password &>/dev/null
+#		/Library/PostgreSQL/12/bin/pg_dump --host ${DBip} --username postgres ${DBname} --blobs --file $BUdatabases/Resolve_${DBname}_${DBip}_$(date +'%m-%d-%Y_%H-%M')_PostgreSQL_db.backup --verbose --format=custom --compress=9 --no-password &>/dev/null
 
 		# Log backup for that IP/DB pair
-		echo -e "`logger -p user.debug -s -t "${DBip} > ${DBname}: Online “ “DaVinci Resolve Database Backup Completed" 2>&1 | tee -a $BUpath/backup_debug.tmp`"
+		echo -e "`logger -p user.debug -s -t "${DBip} > ${DBname}: Online " "DaVinci Resolve Database Backup Completed" 2>&1 | tee -a $BUpath/backup_debug.tmp`"
 
-		# If 4 versions exits of backup, remove oldest backup keep disk usage in-check
-		if [ "$DBbacknum" -gt 3 ];
+		# If 3 versions exits of backup, remove oldest backup keep disk usage in-check
+		if [ "$DBbacknum" -gt 4 ];
 		then
-			echo -e "Rollover: \033[33mRemoving oldest backup of ${DBip}:${DBname} > `ls -1t $BUdatabases | grep Resolve_${DBname}_${DBip}_ | tail -n 1` "
-			## use below rm at your own risk.  Comment out until you test .
-      ls -1t | grep Resolve_${DBname}_${DBip}_ | tail -n 1 | xargs rm -f
-			ls -1t $BUdatabases | grep Resolve_${DBname}_${DBip}_ | tail -n 1
+			echo "Rollover: Removing oldest backups of ${DBip}:${DBname} > `ls -1t $BUdatabases | grep Resolve_${DBname}_${DBip}_ | tail -n 1` "
+
+			echo "Deleted Backups:"
+			ls -1t $BUdatabases | grep Resolve_${DBname}_${DBip}_ | tail -n +4
+			cd $BUdatabases ; ls -t $BUdatabases | grep Resolve_${DBname}_${DBip}_ | tail -n +4 | xargs rm --
+			echo "Remaining Backups:"
+			ls -1t $BUdatabases | grep Resolve_${DBname}_${DBip}_
 		else
-			echo -e "Rollover: Unable to remove oldest backup of ${DBip}:${DBname} > Not enough backups available, skipping removal"
+			echo "Rollover: Unable to remove oldest backup of ${DBip}:${DBname} > Not enough backups available, skipping removal"
+			echo "Remaining Backups:"
+			ls -1t $BUdatabases | grep Resolve_${DBname}_${DBip}_
 		fi
 		echo
-	else
-		# DB Offline, Log Event and Proceed
-		echo -e "*** `logger -p user.error -s -t "${DBip} > {DBname}: OFF-LINE ERROR " "DaVinci Resolve Database Backup Skipped" 2>&1 | tee -a $BUpath/backup_debug.tmp`"
-		echo
-		NOerrors=$((NOerrors+1))
-	fi
-done
+		else
+			# DB Offline, Log Event and Proceed
+			echo -e "*** `logger -p user.error -s -t "${DBip} > {DBname}: OFF-LINE ERROR " "DaVinci Resolve Database Backup Skipped" 2>&1 | tee -a $BUpath/backup_debug.tmp`"
+			echo
+			NOerrors=$((NOerrors+1))
+		fi
+		done
 
 # Check if network share is available, if available rsync local backup to share
-	echo -e "Processing Job $DBtasktotal of $DBtasktotal: Checking Network Share Availability and Backing up the Backups"
+	echo "Processing Job $DBtasktotal of $DBtasktotal: Checking Network Share Availability and Backing up the Backups"
 
 
 if [ -d "$RSYNCpath" ];
